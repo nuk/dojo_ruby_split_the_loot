b7f97f3adbcc2eebd51656588215e8ad45508fca
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2010-10-27 18:55:44 -0200

    código feio começa a ficar recursivo.

--- split_the_loot.rb
+++ split_the_loot.rb
@@ -1,6 +1,51 @@
 class SplitTheLoot
 
+	def validate (treasure, number_of_pirates) 
+		if treasure.empty? then
+			return true
+		end
+		
+		unless treasure.size() >= number_of_pirates then	
+			return false 
+		end
+		
+		# Verify if the sum of the gems can be divided by the pirates
+		unless @sum%number_of_pirates == 0 then	
+			return false 
+		end	
+		return true
+	end
+
 	def split (treasure, number_of_pirates)
-		[[]]
+				
+		@sum = 0
+		treasure.each do |gem| 
+			@sum += gem
+		end
+	
+		unless validate(treasure, number_of_pirates)  
+			return nil
+		end
+				
+		result = []
+		number_of_pirates.times do |i|
+			result << []	
+		end
+		
+		split_rec(treasure, number_of_pirates, 0, 0,result)
+				
+		return result
+	end
+	
+	def split_rec (treasure, number_of_pirates, pirate_index, treasure_index, result)
+		if(pirate_index < number_of_pirates)
+			i = 0
+			while (i < @sum/number_of_pirates )
+				result[pirate_index] << treasure[treasure_index]
+				i+=treasure[treasure_index]
+				treasure_index+=1
+			end
+			split_rec (treasure, number_of_pirates, pirate_index+1, treasure_index, result)			
+		end	
 	end
 end
--- split_the_loot_spec.rb
+++ split_the_loot_spec.rb
@@ -2,11 +2,31 @@ require "split_the_loot"
 
 describe "SplitTheLoot" do
 
+	before :each do
+		@splitter = SplitTheLoot.new
+	end
+	
+	
+	describe "Validation" do
+		it "should not devide when sum is not divisible by the number of  pirates" do
+			@splitter.split([2,3],2).should == nil
+		end
+		
+		it "should not divide when the number of pirates is greater than the number of gems" do
+			@splitter.split([3,3],3).should == nil
+		end
+	end
+
 	it "should return an empty treasure for a single pirate" do
-		treasure = []
-		number_of_pirates = 1
-		splitter = SplitTheLoot.new
-		splitter.split(treasure,number_of_pirates).should == [[]]
+		@splitter.split([],1).should == [[]]
+	end
+	
+	it "should return the treasure for a single pirate" do
+		@splitter.split([2,3],1).should == [[2,3]]
+	end
+	
+	it "should distribuite one gem to each pirate" do
+		@splitter.split([2,2],2).should == [[2],[2]]
 	end
 
 end
