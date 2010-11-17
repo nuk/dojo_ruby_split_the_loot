5a6947e9ede4ead302d3dead14f9766c50284026
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2010-11-17 17:55:19 -0200

    Removemos o indice do array de tesouros

--- split_the_loot.rb
+++ split_the_loot.rb
@@ -1,50 +1,53 @@
 class SplitTheLoot
 
-	def validate (treasure, number_of_pirates) 
+	def validate (treasure) 
 		if treasure.empty? then
 			return true
 		end
 		
-		unless treasure.size() >= number_of_pirates then	
+		unless treasure.size() >= @number_of_pirates then	
 			return false 
 		end
 		
 		# Verify if the sum of the gems can be divided by the pirates
-		unless @sum%number_of_pirates == 0 then	
+		unless @sum%@number_of_pirates == 0 then	
 			return false 
 		end	
 		return true
 	end
 
 	def split (treasure, number_of_pirates)
+
+		@number_of_pirates = number_of_pirates
 				
 		@sum = 0
 		treasure.each do |gem| 
 			@sum += gem
 		end
 	
-		unless validate(treasure, number_of_pirates)  
+		unless validate(treasure)  
 			return nil
 		end
 				
 		result = []
-		number_of_pirates.times do |i|
+		@number_of_pirates.times do |i|
 			result << []	
 		end
 		
-		split_rec(treasure, number_of_pirates, 0, result)
-				
+		
+		split_rec(treasure, 0, result)
 		return result
 	end
 	
-	def split_rec (treasure, number_of_pirates, pirate_index, result)
-		if(pirate_index < number_of_pirates)
+	def split_rec (treasure, pirate_index, result)
+		if(pirate_index < @number_of_pirates)
 			bucket_sum = 0
-			while (bucket_sum < @sum/number_of_pirates )
-				bucket_sum+=treasure[0]
-				result[pirate_index] << treasure.delete_at(0)
+			while (bucket_sum < @sum/@number_of_pirates )
+				gem = treasure.delete_at(0)
+				bucket_sum += gem
+				result[pirate_index] << gem
 			end
-			split_rec (treasure, number_of_pirates, pirate_index+1, result)			
+			split_rec (treasure, pirate_index+1, result)
 		end	
 	end
 end
