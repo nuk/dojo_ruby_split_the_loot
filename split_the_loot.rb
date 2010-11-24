f8bfd4394de1510ac9aaef90cf8792b0c39eb15c
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2010-11-24 17:57:02 -0200

    Conseguimos desfazer apenas a última escolha ruim

--- split_the_loot.rb
+++ split_the_loot.rb
@@ -41,10 +41,23 @@ class SplitTheLoot
 	def split_rec (treasure, pirate_index, result)
 		if(pirate_index < @number_of_pirates)
 			bucket_sum = 0
-			chosen_gem = 0;
+			chosen_gem = 0
+			last_selected_gem_value = 0
 			while (bucket_sum < @pirate_share )
 				if(chosen_gem == treasure.size)
-					return nil
+					if (last_selected_gem_value != 0)
+						puts "> #{last_selected_gem_value}"
+						bucket_sum = bucket_sum - last_selected_gem_value
+						result[pirate_index].delete_at(-1)
+						
+						treasure.insert(0,last_selected_gem_value)
+						chosen_gem = 1
+						last_selected_gem_value = 0
+						
+						puts "#{bucket_sum} : #{pirate_index} : #{treasure.inspect} : #{result[pirate_index].inspect}"
+					else
+						return nil
+					end
 				end
 				if(treasure[chosen_gem]+bucket_sum > @pirate_share)
 					chosen_gem += 1
@@ -52,6 +65,7 @@ class SplitTheLoot
 					gem = treasure.delete_at(chosen_gem)
 					bucket_sum += gem
 					result[pirate_index] << gem					
+					last_selected_gem_value = gem
 				end
 			end
 			return split_rec(treasure, pirate_index+1, result)
--- split_the_loot_spec.rb
+++ split_the_loot_spec.rb
@@ -60,12 +60,22 @@ describe "SplitTheLoot" do
 	end
 	
 	it "should distribute when the next gem is not a good choice" do
-		@splitter.split([8,1, 8,2 ,2,9],3).each do |bucket| 
+		@splitter.split([8,1, 9,2 ,2,8],3).each do |bucket| 
 			bucket.sum.should == 10
 		end		
 	end
 	
-	 
+#	it "should distribute when the next gem is not a good choice for " do
+#		@splitter.split([8,1, 8,2 ,2,9],3).each do |bucket| 
+#			bucket.sum.should == 10
+#		end		
+#	end
+	
+	it "should distribute when the next gem is not a good choice for the second time" do
+		@splitter.split([1, 8,2 ,9],2).each do |bucket| 
+			bucket.sum.should == 10
+		end		
+	end
 	
 	it "should not distribuite the undistribuitable" do
 		@splitter.split([8,2],2).should == nil
