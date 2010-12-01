ce49657c12ac96d900862720110102f895288f02
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2010-12-01 17:40:45 -0200

    removemos a recursão fajuta

--- split_the_loot.rb
+++ split_the_loot.rb
@@ -44,40 +44,37 @@ class SplitTheLoot
 			result << []	
 		end
 		
+		@number_of_pirates.times do |i|
+			unless split_for_pirate(treasure, i, result)
+				return nil
+			end
+		end
 		
-		return split_rec(treasure, 0, result)
+		return result
 	end
 	
-	def split_rec (treasure, pirate_index, result)
-		if(pirate_index < @number_of_pirates)
-			chosen_gem = 0
-			bad_choices_tries = 0
-
-			while (result[pirate_index].sum < @pirate_share )
-				if(chosen_gem == treasure.size)
-					unless (result[pirate_index].empty?)
-						if(bad_choices_tries == treasure.size)
-							return nil
-						end
-						
-						bad_choices_tries += 1
-						treasure.insert(-1,result[pirate_index].delete_at(-1))
-						chosen_gem = 0
-						
-					else
-						return nil
+	def split_for_pirate (treasure, pirate_index, result)
+		chosen_gem = 0
+		bad_choices_tries = 0
+		while (result[pirate_index].sum < @pirate_share )
+			if(chosen_gem == treasure.size)
+				unless (result[pirate_index].empty?)
+					if(bad_choices_tries == treasure.size)
+						return false
 					end
-				end
-				if(treasure[chosen_gem]+result[pirate_index].sum > @pirate_share)
-					chosen_gem += 1
+					bad_choices_tries += 1
+					treasure.insert(-1,result[pirate_index].delete_at(-1))
+					chosen_gem = 0
 				else
-					result[pirate_index] << treasure.delete_at(chosen_gem)
+					return false
 				end
 			end
-
-			return split_rec(treasure, pirate_index+1, result)
-		end	
-	
-		return result
+			if(treasure[chosen_gem]+result[pirate_index].sum > @pirate_share)
+				chosen_gem += 1
+			else
+				result[pirate_index] << treasure.delete_at(chosen_gem)
+			end
+		end
+		return true
 	end
 end
